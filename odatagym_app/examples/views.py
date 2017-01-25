303bd4f81784f67b6361ce26e54409ce5cf4dd8d
Alessandro <kscope358@gmail.com>
2017-01-25 14:50:57 +0100

    Aggiunti esempi bandalarga

--- odatagym_app/examples/views.py
+++ odatagym_app/examples/views.py
@@ -7,6 +7,20 @@ def get_sardinia_svg_example(request):
     return render(request, 'sardinia_svg.html')
 
 
+def get_banda_larga_2018(request):
+    return render(request, 'bandalarga2018.html')
+
+def get_banda_larga_2020(request):
+    return render(request, 'bandalarga2020.html')
+
+
+def get_banda_larga_2015(request):
+    return render(request, 'bandalarga2015.html')
+
+def get_banda_larga(request):
+    return render(request, 'bandalarga.html')
+
+
 def get_sardinia_drugstores(request):
     return render(request, 'markers.html',
                   context={'google_api_key': GOOGLE_API_KEY})
